49654d7baf4960ac03cb6956cd37c06c2c01b4f4
Carrel MEZATSONG <meztsacar@gmail.com>
2024-10-14 22:19:47 +0100

    Test gitHooks

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -1,15 +1,15 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-abstract class  LocaleKeys {
+abstract class LocaleKeys {
   static const app_title = 'app_title';
   static const common_next = 'common.next';
   static const common_gender_male = 'common.gender_male';
   static const common_gender_female = 'common.gender_female';
   static const common_gender_other = 'common.gender_other';
   static const welcome_screen_greeting = 'welcome_screen.greeting';
-  static const welcome_screen_on_the_world_coolest_chat = 'welcome_screen.on_the_world_coolest_chat';
+  static const welcome_screen_on_the_world_coolest_chat =
+      'welcome_screen.on_the_world_coolest_chat';
   static const welcome_screen_note = 'welcome_screen.note';
   static const image_service_choose_camera = 'image_service.choose_camera';
   static const image_service_choose_gallery = 'image_service.choose_gallery';
-
 }
