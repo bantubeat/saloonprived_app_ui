cc511f806e6722a02b0c50e7f93ae0326265fdaf
ARROKO <kemgang605@gmail.com>
2024-10-16 16:29:33 +0100

    ticket-0.3/notification-screen update

--- lib/src/screens/notification_screen/widgets/notification_card_item.dart
+++ lib/src/screens/notification_screen/widgets/notification_card_item.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:saloonprived_app/generated/locale_keys.g.dart';
+import 'package:saloonprived_app/src/config/app_colors.dart';
 
 class NotificationCardItem extends StatelessWidget {
   const NotificationCardItem({
@@ -82,9 +83,9 @@ class NotificationCardItem extends StatelessWidget {
     if (parts.length == 2) {
       return [
         TextSpan(text: parts[0]),
-        TextSpan(
+        const TextSpan(
           text: 'En Direct',
-          style: const TextStyle(color: Colors.red),
+          style: TextStyle(color: AppColors.bantubeatPrimary),
         ),
         TextSpan(text: parts[1]),
       ];
