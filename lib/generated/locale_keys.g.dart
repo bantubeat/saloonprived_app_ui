eddae1a8ff48710b2ffb7c5056b05e3a4cce4761
ARROKO <kemgang605@gmail.com>
2024-10-16 02:24:05 +0100

    ticket-0.3/notification-screen add screen

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -1,6 +1,6 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-abstract class LocaleKeys {
+abstract class  LocaleKeys {
   static const app_title = 'app_title';
   static const common_next = 'common.next';
   static const common_gender_male = 'common.gender_male';
@@ -8,8 +8,7 @@ abstract class LocaleKeys {
   static const common_gender_other = 'common.gender_other';
   static const common_preview = 'common.preview';
   static const welcome_screen_greeting = 'welcome_screen.greeting';
-  static const welcome_screen_on_the_world_coolest_chat =
-      'welcome_screen.on_the_world_coolest_chat';
+  static const welcome_screen_on_the_world_coolest_chat = 'welcome_screen.on_the_world_coolest_chat';
   static const welcome_screen_note = 'welcome_screen.note';
   static const image_service_choose_camera = 'image_service.choose_camera';
   static const image_service_choose_gallery = 'image_service.choose_gallery';
