d2cb6b61f2178531dd85ae22d2c1c279c2fd7e49
fofack <74138855+fofack@users.noreply.github.com>
2024-10-20 05:58:11 +0100

    ticket-2.4/restriction_page: Restriction page added (#8)
    Co-authored-by: MEZATSONG TSAFACK Carrel <42788152+Mezatsong@users.noreply.github.com>

--- lib/generated/locale_keys.g.dart
+++ lib/generated/locale_keys.g.dart
@@ -1,53 +1,40 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-abstract class LocaleKeys {
+abstract class  LocaleKeys {
   static const app_title = 'app_title';
   static const common_next = 'common.next';
   static const common_gender_male = 'common.gender_male';
   static const common_gender_female = 'common.gender_female';
   static const common_gender_other = 'common.gender_other';
+  static const common_preview = 'common.preview';
   static const welcome_screen_greeting = 'welcome_screen.greeting';
-  static const welcome_screen_on_the_world_coolest_chat =
-      'welcome_screen.on_the_world_coolest_chat';
+  static const welcome_screen_on_the_world_coolest_chat = 'welcome_screen.on_the_world_coolest_chat';
   static const welcome_screen_note = 'welcome_screen.note';
   static const image_service_choose_camera = 'image_service.choose_camera';
   static const image_service_choose_gallery = 'image_service.choose_gallery';
-  static const saloon_for_you_no_subcription_screen_dicover_saloon =
-      'saloon_for_you_no_subcription_screen.dicover_saloon';
-  static const saloon_for_you_no_subcription_screen_saloon_for_you =
-      'saloon_for_you_no_subcription_screen.saloon_for_you';
-  static const saloon_for_you_no_subcription_screen_subscription_prompt =
-      'saloon_for_you_no_subcription_screen.subscription_prompt';
-  static const saloon_for_you_no_subcription_screen_discover_creators =
-      'saloon_for_you_no_subcription_screen.discover_creators';
-  static const saloon_for_you_no_subcription_screen_subscribe_to_creators =
-      'saloon_for_you_no_subcription_screen.subscribe_to_creators';
-  static const profile_create_user_profile_screen_title =
-      'profile_create_user_profile_screen.title';
-  static const profile_create_user_profile_screen_subscriptions_count =
-      'profile_create_user_profile_screen.subscriptions_count';
-  static const profile_create_user_profile_screen_followers_count =
-      'profile_create_user_profile_screen.followers_count';
-  static const profile_create_user_profile_screen_cancel_button =
-      'profile_create_user_profile_screen.cancel_button';
-  static const profile_create_user_profile_screen_add_button =
-      'profile_create_user_profile_screen.add_button';
+  static const saloon_for_you_no_subcription_screen_dicover_saloon = 'saloon_for_you_no_subcription_screen.dicover_saloon';
+  static const saloon_for_you_no_subcription_screen_saloon_for_you = 'saloon_for_you_no_subcription_screen.saloon_for_you';
+  static const saloon_for_you_no_subcription_screen_subscription_prompt = 'saloon_for_you_no_subcription_screen.subscription_prompt';
+  static const saloon_for_you_no_subcription_screen_discover_creators = 'saloon_for_you_no_subcription_screen.discover_creators';
+  static const saloon_for_you_no_subcription_screen_subscribe_to_creators = 'saloon_for_you_no_subcription_screen.subscribe_to_creators';
+  static const profile_create_user_profile_screen_title = 'profile_create_user_profile_screen.title';
+  static const profile_create_user_profile_screen_subscriptions_count = 'profile_create_user_profile_screen.subscriptions_count';
+  static const profile_create_user_profile_screen_followers_count = 'profile_create_user_profile_screen.followers_count';
+  static const profile_create_user_profile_screen_cancel_button = 'profile_create_user_profile_screen.cancel_button';
+  static const profile_create_user_profile_screen_add_button = 'profile_create_user_profile_screen.add_button';
   static const register_screen_header_text = 'register_screen.header_text';
   static const register_screen_title = 'register_screen.title';
   static const register_screen_description = 'register_screen.description';
-  static const register_screen_sign_up_google =
-      'register_screen.sign_up_google';
+  static const register_screen_sign_up_google = 'register_screen.sign_up_google';
   static const register_screen_sign_up_email = 'register_screen.sign_up_email';
   static const register_screen_email_hint = 'register_screen.email_hint';
   static const register_screen_password_hint = 'register_screen.password_hint';
   static const register_screen_sign_up = 'register_screen.sign_up';
-  static const register_screen_new_to_featlink =
-      'register_screen.new_to_featlink';
+  static const register_screen_new_to_featlink = 'register_screen.new_to_featlink';
   static const register_screen_login = 'register_screen.login';
   static const login_screen_title = 'login_screen.title';
   static const login_screen_description = 'login_screen.description';
-  static const login_screen_login_with_google =
-      'login_screen.login_with_google';
+  static const login_screen_login_with_google = 'login_screen.login_with_google';
   static const login_screen_login = 'login_screen.login';
   static const login_screen_not_register = 'login_screen.not_register';
   static const login_screen_sign_up = 'login_screen.sign_up';
@@ -60,36 +47,38 @@ abstract class LocaleKeys {
   static const login_screen_second_line_text = 'login_screen.second_line_text';
   static const login_screen_account_text = 'login_screen.account_text';
   static const login_screen_channel_text = 'login_screen.channel_text';
-  static const login_screen_support_creators_text =
-      'login_screen.support_creators_text';
-  static const login_screen_partners_program_text =
-      'login_screen.partners_program_text';
+  static const login_screen_support_creators_text = 'login_screen.support_creators_text';
+  static const login_screen_partners_program_text = 'login_screen.partners_program_text';
   static const login_screen_year_text = 'login_screen.year_text';
   static const login_screen_cookies_text = 'login_screen.cookies_text';
   static const login_screen_privacy_text = 'login_screen.privacy_text';
-  static const login_screen_terms_conditions_text =
-      'login_screen.terms_conditions_text';
+  static const login_screen_terms_conditions_text = 'login_screen.terms_conditions_text';
   static const notification_screen_title = 'notification_screen.title';
   static const notification_screen_time = 'notification_screen.time';
   static const notification_screen_message = 'notification_screen.message';
   static const my_bottom_navigation_bar_home = 'my_bottom_navigation_bar.home';
-  static const my_bottom_navigation_bar_messages =
-      'my_bottom_navigation_bar.messages';
-  static const my_bottom_navigation_bar_bantubeat =
-      'my_bottom_navigation_bar.bantubeat';
-  static const my_bottom_navigation_bar_profil =
-      'my_bottom_navigation_bar.profil';
-  static const my_bottom_navigation_bar_music =
-      'my_bottom_navigation_bar.music';
+  static const my_bottom_navigation_bar_messages = 'my_bottom_navigation_bar.messages';
+  static const my_bottom_navigation_bar_bantubeat = 'my_bottom_navigation_bar.bantubeat';
+  static const my_bottom_navigation_bar_profil = 'my_bottom_navigation_bar.profil';
+  static const my_bottom_navigation_bar_music = 'my_bottom_navigation_bar.music';
   static const my_bottom_navigation_bar_beat = 'my_bottom_navigation_bar.beat';
-  static const my_bottom_navigation_bar_settings =
-      'my_bottom_navigation_bar.settings';
-  static const my_bottom_navigation_bar_feat_link =
-      'my_bottom_navigation_bar.feat_link';
-  static const my_bottom_navigation_bar_log_out =
-      'my_bottom_navigation_bar.log_out';
+  static const my_bottom_navigation_bar_settings = 'my_bottom_navigation_bar.settings';
+  static const my_bottom_navigation_bar_feat_link = 'my_bottom_navigation_bar.feat_link';
+  static const my_bottom_navigation_bar_log_out = 'my_bottom_navigation_bar.log_out';
   static const my_app_bar_for_you_text = 'my_app_bar.for_you_text';
   static const my_app_bar_for_discover_text = 'my_app_bar.for_discover_text';
+
+  static const saloon_for_you_have_subcription_screen_advertisement_title = 'saloon_for_you_have_subcription_screen.advertisement_title';
+  static const saloon_for_you_have_subcription_screen_learn_more = 'saloon_for_you_have_subcription_screen.learn_more';
+  static const saloon_for_you_have_subcription_screen_tip_label = 'saloon_for_you_have_subcription_screen.tip_label';
+  static const saloon_for_you_have_subcription_screen_suggestions_label = 'saloon_for_you_have_subcription_screen.suggestions_label';
+  static const create_profile_restriction_page_screen_app_bar_title = 'create_profile_restriction_page_screen.app_bar_title';
+  static const create_profile_restriction_page_screen_slogan = 'create_profile_restriction_page_screen.slogan';
+  static const create_profile_restriction_page_screen_adult_category_text = 'create_profile_restriction_page_screen.adult_category_text';
+  static const create_profile_restriction_page_screen_desable_category = 'create_profile_restriction_page_screen.desable_category';
+  static const create_profile_restriction_page_screen_subscribers_content = 'create_profile_restriction_page_screen.subscribers_content';
+  static const create_profile_restriction_page_screen_preferences = 'create_profile_restriction_page_screen.preferences';
+
   static const search_screen_find_creator = 'search_screen.find_creator';
   static const search_screen_subscribers_count =
       'search_screen.subscribers_count';
@@ -105,4 +94,5 @@ abstract class LocaleKeys {
       'saloon_for_you_have_subcription_screen.tip_label';
   static const saloon_for_you_have_subcription_screen_suggestions_label =
       'saloon_for_you_have_subcription_screen.suggestions_label';
+
 }
