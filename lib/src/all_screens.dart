f0a1738a95ab596acebf45911635fff8d6dbc995
bertho <berthoazeye21@mail.com>
2024-10-21 16:31:28 +0100

    ticket-24/blocked-user-activity add page

--- lib/src/all_screens.dart
+++ lib/src/all_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart' show Widget;
 import 'package:saloonprived_app/src/screens/_rooms/paid_connected_user_saloon_config_price_screen/paid_connected_user_saloon_config_price_screen.dart';
 
+import 'screens/_activity/blocked_user_activity_screen/blocked_user_activity_screen.dart';
 import 'screens/_rooms/free_connected_user_saloon_screen/free_connected_user_saloon_screen.dart';
 import 'screens/_rooms/paid_connected_user_saloon_no_config_price_screen.dart';
 import 'screens/_sallon_for_you/saloon_for_you_have_subcription/saloon_for_you_have_subcription_screen.dart';
@@ -46,6 +47,8 @@ final Map<String, Widget Function()> allScreens = {
   'search_screen.dart': () => const SearchScreen(),
   'paid_connected_user_saloon_no_config_price_screen.dart': () =>
       const PaidConnectedUserSaloonNoConfigPriceScreen(),
+  'blocked_user_activity_screen.dart': () =>
+      const BlockedUserActivityScreen(),
 };
 
 // Ajoutez d'autres chemins de fichiers ici
