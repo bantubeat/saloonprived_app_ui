2150b280fb2798e5472ae9f3c82d4a139a424ac1
ARROKO <kemgang605@gmail.com>
2024-10-22 11:07:47 +0100

    ticket-9.2/paid_connected_user_saloon_of_5_content_free add page

--- lib/src/all_screens.dart
+++ lib/src/all_screens.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart' show Widget;
 import 'package:saloonprived_app/src/screens/_rooms/paid_connected_user_saloon_config_price_screen/paid_connected_user_saloon_config_price_screen.dart';
+import 'package:saloonprived_app/src/screens/_rooms/paid_connected_user_saloon_of_5_content_free/paid_connected_user_saloon_of_5_content_free_screen.dart';
 
 import 'screens/_rooms/free_connected_user_saloon_screen/free_connected_user_saloon_screen.dart';
 import 'screens/_rooms/paid_connected_user_saloon_no_config_price_screen.dart';
@@ -46,6 +47,8 @@ final Map<String, Widget Function()> allScreens = {
   'search_screen.dart': () => const SearchScreen(),
   'paid_connected_user_saloon_no_config_price_screen.dart': () =>
       const PaidConnectedUserSaloonNoConfigPriceScreen(),
+  'paid_connected_user_saloon_of_5_content_free_screen.dart': () =>
+      const PaidConnectedUserSaloonOf5ContentFree(),
 };
 
 // Ajoutez d'autres chemins de fichiers ici
