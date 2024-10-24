c2d5a6bea6e9da880194d284826118f5ee9be674
bertho <berthoazeye21@mail.com>
2024-10-24 10:37:47 +0100

    ticket-6/share-madal add modal

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,8 @@
 import 'package:saloonprived_app/generated/locale_keys.g.dart';
+import 'package:saloonprived_app/src/screens/welcome_screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:saloonprived_app/src/screens/welcome_screen/welcome_screen.dart';
 import 'config/app_themes.dart';
-import 'screens/discover_lounge/discover_lounge_non_adults_creators_screen/discover_lounge_non_adults_creators_screen.dart';
 
 /// The Widget that configures your application.
 class SaloonprivedApp extends StatelessWidget {
@@ -22,7 +21,7 @@ class SaloonprivedApp extends StatelessWidget {
       darkTheme: AppThemes.darkTheme,
       // Remplace `WelcomeScreen` par ta SCREEN pour la tester et à la fin remet
       // `WelcomeScreen` comme tu vois.
-      home: DiscoverLoungeNonAdultsCreatorsScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
