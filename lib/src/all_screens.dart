c1066a672215b2aa1597178cdd83d7c2ccca1154
ARROKO <84386958+ARROKO@users.noreply.github.com>
2024-11-09 08:36:17 +0100

    ticket-9.2/paid_connected_user_saloon_of_5_content_free-2 update

--- lib/src/all_screens.dart
+++ lib/src/all_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart' show Widget;
+import 'package:saloonprived_app/src/screens/_rooms/paid_connected_user_saloon_of_5_content_free/paid_connected_user_saloon_of_5_content_free.dart';
 import 'package:saloonprived_app/src/screens/discover_lounge/discover_saloon_adult_screen/discover_saloon_adult_screen.dart';
 import 'package:saloonprived_app/src/screens/profile_creation_category/profile_creation_category_screen.dart';
 
@@ -40,7 +41,6 @@ import 'screens/_dashboard/access_price_user_dashboard_screen/access_price_user_
 import 'screens/_rooms/free_connected_user_saloon_screen/free_connected_user_saloon_screen.dart';
 
 import 'screens/_rooms/opend_post_saloon_free_and_paid_screen/opend_post_saloon_free_and_paid_screen.dart';
-import 'screens/_rooms/paid_connected_user_saloon_of_5_content_free/paid_connected_user_saloon_of_5_content_free.dart';
 import 'screens/_rooms/paid_connected_user_saloon_unverify_kyc_screen/paid_connected_user_saloon_unverify_kyc_screen.dart';
 
 import 'screens/_rooms/paid_connected_user_saloon_no_access_screen/paid_connected_user_saloon_no_access_screen.dart';
